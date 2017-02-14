ebb52d165ceca4038b254a0ba3b44bc649cd465b
Alexis Woo <alexis.woo@doctolib.fr>
2017-02-14 10:44:43 +0100

    Run all tests within a file

--- extension.js
+++ extension.js
@@ -6,34 +6,19 @@ var vscode = require('vscode');
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ruby-test" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.runTest', function () {
-        // The code you place here will be executed every time your command is executed
+    const disposable = vscode.commands.registerCommand('extension.runSingleTest', function () {
+        runCommandFromConfigurationKey('testSingleFileCommand')
+    });
 
-        // Display a message box to the user
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) return;
-
-        const commandPattern = vscode
-            .workspace
-            .getConfiguration('rubyTest')
-            .get('testCommand')
-        const command = commandPattern
-            .replace('$FILE', vscode.workspace.asRelativePath(editor.document.fileName))
-            .replace('$LINE', editor.selection.active.line + 1)
-        
-        const terminal = vscode.window.createTerminal();
-        terminal.sendText(command);
-        terminal.show();
+    const allTestDisposable = vscode.commands.registerCommand('extension.runAllTests', function() {
+        runCommandFromConfigurationKey('testAllFileCommand')
     });
 
     context.subscriptions.push(disposable);
+    context.subscriptions.push(allTestDisposable);
 }
 exports.activate = activate;
 
@@ -41,3 +26,20 @@ exports.activate = activate;
 function deactivate() {
 }
 exports.deactivate = deactivate;
+
+function runCommandFromConfigurationKey(configurationKey) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+
+    const commandPattern = vscode
+        .workspace
+        .getConfiguration('rubyTest')
+        .get(configurationKey);
+    const command = commandPattern
+        .replace('$FILE', vscode.workspace.asRelativePath(editor.document.fileName))
+        .replace('$LINE', editor.selection.active.line + 1);
+
+    const terminal = vscode.window.createTerminal();
+    terminal.sendText(command);
+    terminal.show();
+}
